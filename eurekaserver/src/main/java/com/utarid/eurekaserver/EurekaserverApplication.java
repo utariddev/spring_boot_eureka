5ee6beeb2e3d72fa995b9c437280a808ed75dd96
utarid <utarid@protonmail.com>
2023-07-12 23:13:12 +0200

    added eureka server config

--- eurekaserver/src/main/java/com/utarid/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/utarid/eurekaserver/EurekaserverApplication.java
@@ -2,12 +2,14 @@ package com.utarid.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaserverApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaserverApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
 
 }
