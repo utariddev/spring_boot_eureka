47740db0c7f2b8c69897d228f3554736cda26693
utarid <utarid@protonmail.com>
2023-07-14 16:29:28 +0200

    added testWithoutFeign test method

--- client/src/test/java/org/utarid/client/ClientApplicationTests.java
+++ client/src/test/java/org/utarid/client/ClientApplicationTests.java
@@ -1,13 +1,46 @@
 package org.utarid.client;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class ClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private EurekaClient eurekaClient;
 
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testWithFeign() {
+
+    }
+    @Test
+    void testWithoutFeign() {
+        InstanceInfo service = eurekaClient
+                .getApplication("spring-cloud-eureka-client")
+                .getInstances()
+                .get(0);
+
+        String hostName = service.getHostName();
+        int port = service.getPort();
+
+        System.out.println("hostName:" + hostName);
+        System.out.println("port :" + port);
+
+        String url = "http://" + hostName + ":" + port + "/greeting";
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println("response :" + response);
+    }
 }
